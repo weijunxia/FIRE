dbf3858e328d5c2c6d645ee74f6550d86a38632e
Wei Jun Xia <weijunx1224@gmail.com>
2021-09-02 12:00:38 -0400

    plaid link success

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,6 @@
 import React, { useState, useEffect, Pressable } from 'react'
 import { Route, Switch } from 'react-router-dom'
 import axios from 'axios'
-import {
-  usePlaidLink,
-  PlaidLinkOptions,
-  PlaidLinkOnSuccess,
-  PlaidLink
-} from 'react-plaid-link'
 import { BASE_URL } from './global'
 // components
 import NavBarComp from './components/NavBar/NavBar'
@@ -19,13 +13,7 @@ import 'bootstrap/dist/css/bootstrap.min.css'
 import logo from './images/fatFIRE.png'
 
 function App(props) {
-  const [linkToken, setLinkToken] = useState('')
   const [goals, setGoals] = useState([])
-
-  const requestLinkToken = async () => {
-    const response = await axios.get(`${BASE_URL}/create-link-token`)
-    setLinkToken(response.data.linkToken)
-  }
 
   useEffect(() => {
     async function getGoal() {
@@ -33,18 +21,7 @@ function App(props) {
       setGoals(res.data.results)
     }
     // getGoal()
-    requestLinkToken()
   }, [])
-
-  const onExit = (error, metadata) => console.log('onExit', error, metadata)
-
-  const onEvent = (eventName, metadata) => {
-    console.log('onEvent', eventName, metadata)
-  }
-
-  const onSuccess = (token, metadata) => {
-    console.log('onSuccess', token, metadata)
-  }
 
   return (
     <div className="App">
--- client/src/components/PlaidComponents/PlaidLinkComp.js
+++ client/src/components/PlaidComponents/PlaidLinkComp.js
@@ -7,19 +7,39 @@ import {
   PlaidLinkOnSuccess,
   PlaidLink
 } from 'react-plaid-link'
-import plaidClient from '../../../../routes/AppRouter'
+import { logSuccess } from 'util'
 import { BASE_URL } from '../../global'
+import './plaidlinkcomponent.css'
 
-export default async function PlaidLinkComponent() {
+export default function PlaidLinkComponent() {
   const [linkToken, setLinkToken] = useState('')
+  const [accessToken, setAccessToken] = useState('')
+  const [accounts, setAccounts] = useState([])
 
   const requestLinkToken = async () => {
     const response = await axios.get(`${BASE_URL}/create-link-token`)
     setLinkToken(response.data.linkToken)
   }
 
+  const insertAccounts = async (accounts) => {
+    const res = await axios.post(`${BASE_URL}/accounts`, accounts)
+    setAccounts([...accounts, ...res.data])
+  }
+
+  const getAccounts = async () => {
+    const res = await axios.get(`${BASE_URL}/accounts`)
+    setAccounts(res.data)
+  }
+
+  const exchangePublicToken = async () => {}
+
+  const now = moment()
+  const today = now.format(`YYYY-MM-DD`)
+  const thirtyDaysAgo = now.subtract(30, 'days').format('YYYY-MM-DD')
+
   useEffect(() => {
     requestLinkToken()
+    getAccounts()
   }, [])
 
   const onExit = (error, metadata) => console.log('onExit', error, metadata)
@@ -29,33 +49,27 @@ export default async function PlaidLinkComponent() {
   }
 
   const onSuccess = async (token, metadata) => {
-    console.log('onSuccess', token, metadata)
+    console.log(token, metadata)
+    const accounts = metadata.accounts.map((account) => ({
+      plaidId: account.id,
+      accountName: account.name,
+      accountSubtype: account.subtype,
+      accountType: account.type,
+      accessToken: token,
+      institutionId: metadata.institution.institution_id,
+      institutionName: metadata.institution.name
+    }))
+    await insertAccounts(accounts)
   }
-  const response = await plaidClient.itemPublicTokenExchange({ linkToken })
 
-  const access_token = response.access_token
+  const getTransactions = async (accountId) => {
+    const res = await axios.get(`${BASE_URL}/transactions/${accountId}`)
+  }
 
-  const accounts_response = await plaidClient.accountsGet({ access_token })
-
-  const accounts = accounts_response.accounts
-
-  const now = moment()
-  const today = now.format(`YYYY-MM-DD`)
-  const thirtyDaysAgo = now.subtract(30, 'days').format('YYYY-MM-DD')
-
-  const response2 = await plaidClient.transactionsGet({
-    access_token,
-    start_date: thirtyDaysAgo,
-    end_date: today
-  })
-  const transactions = response2.transactions
-  console.log(
-    `You have ${transactions.length} transactions form the last thirty days`
-  )
   return (
     <div>
       <PlaidLink
-        className="Custom Button"
+        className="CustomButton"
         style={{ padding: '20px', fontSize: '16px', curor: 'pointer' }}
         token={linkToken}
         onExit={onExit}
@@ -64,6 +78,14 @@ export default async function PlaidLinkComponent() {
       >
         Connect to your bank account
       </PlaidLink>
+      {accounts.map((account) => (
+        <div key={account._id}>
+          <h1>{account.accountName}</h1>
+          <button onClick={() => getTransactions(account._id)}>
+            Check Transactions
+          </button>
+        </div>
+      ))}
     </div>
   )
 }
